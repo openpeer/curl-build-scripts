45d70a69812060870008abe480bbf2cc71d7eb49
Robin Raymond <robin@hookflash.com>
2015-03-09 00:11:03 -0400

    - added prepare script

---
+++ prepare.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+echo
+echo Preparing curl...
+echo
+
+preparelink()
+{
+	if [ ! -d "$1" ]; then
+		echo ERROR: Path to link does not exist \"$1\" !
+	fi
+	pushd $1 > /dev/null
+	if [ ! -d "$3" ]; then
+		echo ERROR: Link destination is not found \"$3\" inside \"$1\" !
+		popd > /dev/null
+		exit -1
+	fi
+	if [ ! -h "$2" ]; then
+		echo
+		echo In path \"$1\" creating symbolic link \"$2\" pointing to \"$3\"...
+		echo
+		ln -s $3 $2
+		if [ $? -ne 0 ]; then
+			failure=$?
+			echo Faield to create symbolic link
+			popd > /dev/null
+			exit $failure
+		fi
+	fi
+	popd > /dev/null
+}
+
+./build_curl
+
+preparelink "curl" "curl" "ios-appstore/include"
+
+
+echo
+echo Curl ready.
+echo
